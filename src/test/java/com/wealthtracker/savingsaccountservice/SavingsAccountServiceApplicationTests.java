dd53abcc2241c4cc9eb0dd3109f297aa3f833423
Kevin Dunne <kevinmdunne@users.noreply.github.com>
2024-02-24 21:55:28 +0000

    initial commit of the savings account service

---
+++ src/test/java/com/wealthtracker/savingsaccountservice/SavingsAccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wealthtracker.savingsaccountservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class SavingsAccountServiceApplicationTests {
+
+//	@Test
+	void contextLoads() {
+	}
+
+}
